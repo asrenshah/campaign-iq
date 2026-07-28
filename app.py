b4710d8065d33deb3343e830bdf219ff2e552b87
unknown <asrenshah@gmail.com>
2026-07-29 05:34:00 +0800

    Force port 8080

--- app.py
+++ app.py
@@ -292,5 +292,5 @@ def export_csv():
 # RUN SERVER
 # ============================================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
